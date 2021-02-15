e7465c8776ccd1a2a7e57dad22852569afd53b4b
Nikolay Chechulin <nchechulin@aol.com>
2021-02-15 12:38:11 +0300

    draw all cells on screen

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::time::Duration;
 use ggez::graphics::{DrawMode, Rect};
 
 // width and height
-const GRID_SIZE: (i16, i16) = (80, 60);
-const GRID_CELL_SIZE: (i16, i16) = (10, 10);
+const GRID_SIZE: (i16, i16) = (40, 30);
+const GRID_CELL_SIZE: (i16, i16) = (20, 20);
 const CELL_PADDING: i16 = 1;
 
 const SCREEN_SIZE: (f32, f32) = (
@@ -19,16 +19,40 @@ struct GameState {
     meshes: Vec<graphics::Mesh>,
 }
 
+impl GameState {
+    fn new(ctx: &mut Context) -> GameResult<GameState> {
+        let mb = &mut graphics::MeshBuilder::new();
+
+        for row_num in 0..(GRID_SIZE.1) {
+            for col_num in 0..(GRID_SIZE.0) {
+                let x_pos = col_num * GRID_CELL_SIZE.0 + col_num * CELL_PADDING;
+                let y_pos = row_num * GRID_CELL_SIZE.1 + row_num * CELL_PADDING;
+
+                let rect = graphics::Rect::new(x_pos as f32, y_pos as f32, GRID_CELL_SIZE.0 as f32, GRID_CELL_SIZE.1 as f32);
+
+                mb.rectangle(
+                    DrawMode::fill(),
+                    rect,
+                    graphics::Color::WHITE,
+                );
+            }
+        }
+
+        Ok(GameState {
+            meshes: vec![mb.build(ctx)?]
+        })
+    }
+}
+
 impl ggez::event::EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        // self.dt = timer::delta(ctx);
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let rect = graphics::Rect::new(450.0, 450.0, 50.0, 50.0);
-        let r1 =
-            graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), rect, graphics::Color::WHITE)?;
-        graphics::draw(ctx, &r1, graphics::DrawParam::default())?;
+        for cell_mesh in &self.meshes {
+            graphics::draw(ctx, cell_mesh, graphics::DrawParam::default())?;
+        }
+
         graphics::present(ctx)?;
         Ok(())
     }
@@ -46,7 +70,7 @@ fn main() {
 
 
     // Next we create a new instance of our GameState struct, which implements EventHandler
-    let mut state = GameState { meshes: vec![] };
+    let mut state = GameState::new(&mut ctx).unwrap();
 
     // And finally we actually run our game, passing in our context and state.
     event::run(ctx, events_loop, state)
